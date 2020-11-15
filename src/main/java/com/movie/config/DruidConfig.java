d24e796a1d5a9f6bd6fafd4a5ad6436cdf1978c9
china-ktp <2693646112@qq.com>
2020-11-15 15:23:49 +0800

    修改druid配置文件

--- src/main/java/com/movie/config/DruidConfig.java
+++ src/main/java/com/movie/config/DruidConfig.java
@@ -40,7 +40,6 @@ public class DruidConfig {
         //允许谁能访问
         initParameters.put("allow","");
         //禁止谁能访问
-        //initParameters.put("deny","192.168.11.11");
         initParameters.put("deny","192.168.11.11");
         //设置初始化参数
         bean.setInitParameters(initParameters);
